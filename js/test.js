71aad8c6581aac9a7439dcb952a8d266d50c135d
Horia Dragomir <horia@hdragomir.com>
2011-08-20 03:49:08 +0200

    right, those are the tests I am talking about

---
+++ js/test.js
@@ -0,0 +1,70 @@
+
+(function(){
+    
+    if(! /test/i.test(window.location.search)){
+        return;
+    }
+    var cash = Object.create(window.cash);
+    cash.reset = function(){this.spendings = []; this.ins = []; this.ballance = 0;};
+    
+    var is = function(assert, onfailMessage){
+        if(assert)
+            console.log('.');
+        else
+            throw onfailMessage;
+    };
+    
+    var month = '201108';
+    
+    cash.ballance = 10;
+    cash.spend({amount: 5});
+    is(cash.ballance === 5, "Ballance not updating correctly");
+    
+    cash.reset();
+    is(cash.ballance === 0, "Reset not working on ballance");
+    
+    cash.spend({amount: 5});
+    cash.reset();
+    is(cash.spendings.length === 0, "Reset not working on spendings");
+    
+    var mockSpend = cash._prepare({amount: 5});
+    is(mockSpend.key === month, "Key not being calculated right");
+    
+    cash.spend({amount: 5, comment: "A comment"});
+    is(cash.spendings[0].comment === "A comment", "Comments not being saved properly");
+    
+    cash.reset();
+    cash.spend({amount: 10});
+    is(cash.month(month) !== undefined, "Month api not working");
+    
+    cash.reset();
+    var spent = Math.floor(Math.random() * 18),
+        earned = Math.floor(Math.random() * 20);
+    cash.spend({amount: spent});
+    cash.add({amount: earned});
+    var monthAggregate = cash.month(month);
+    is( monthAggregate.spendings !== undefined
+       && monthAggregate.spendings.length === 1
+       && (
+          monthAggregate.spendings[0].amount === spent
+       )
+       && cash.month(month).ins !== undefined
+       && monthAggregate.ins.length === 1
+       && monthAggregate.ins[0].amount === earned
+       
+       && monthAggregate.earned !== undefined
+       && monthAggregate.earned === earned, "Month api not returning a proper object");
+    
+    is(monthAggregate.spendings[0].amount === spent
+       && monthAggregate.spent !== undefined
+       && monthAggregate.spent === spent, "Month spendings are not calculated correcly");
+    
+    is(monthAggregate.ins[0].amount === earned
+       && monthAggregate.earned !== undefined
+       && monthAggregate.earned === earned, "Month earnings are not calculated correcly");
+    
+    is(monthAggregate.ballance === earned - spent, "Month ballance is not correct");
+    
+    
+    
+}());
